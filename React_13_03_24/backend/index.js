a58efea88cd7758ada6fa7d4465d5c6a7d6b26e6
Dev Kayasth <devkayasth1611@gmail.com>
2024-03-18 22:47:40 +0530

    server-connect

--- React_13_03_24/backend/index.js
+++ React_13_03_24/backend/index.js
@@ -1,2 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express"); // creating the server
+const app = express();
+const mongoose = require("mongoose"); // connecting to database
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/production")
+  .then((sucess) => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.listen(3000, (err, success) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server is running on port=>"+3000);
+  }
+}); //where my services will called()
